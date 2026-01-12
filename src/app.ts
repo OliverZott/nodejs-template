900f853c3b572df39fce9c8b3d59f813834796d0
Oliver Zott <zott_oliver@web.de>
2026-01-12 20:26:13 +0100

    feat: 404 error page

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,11 @@ const port = 3000;
 
 app.use("/api", router)
 
+//Fallback for wrong routes
+app.use((req, res, next) => {
+    res.status(404).send("<h1>Page not found</h1>");
+});
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
